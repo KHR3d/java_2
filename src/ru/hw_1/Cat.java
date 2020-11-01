419e482058b134f1523f5aeaa89d2f90f5f46327
Vladi Malov <vladi.prog@bk.ru>
2020-11-01 20:40:41 +0300

    Initial commit

---
+++ src/ru/hw_1/Cat.java
@@ -0,0 +1,38 @@
+package ru.hw_1;
+
+public class Cat implements IRunning, IJumping {
+
+    private String name;
+    private int runRange;
+    private int jumpHeight;
+
+        public Cat(String name, int runRange, int jumpHeight) {
+            this.name = name;
+            this.runRange = runRange;
+            this.jumpHeight = jumpHeight;
+        }
+
+                @Override
+                public void jumping() {
+                    for (int i = 0; i < wallHeight.length ; i++) {
+                        if (jumpHeight > wallHeight[i]) {
+                            System.out.println(name + " перепрыгнул припятствие");
+                        } else {
+                            System.out.println(name + " не смог :(");
+                            break;
+                        }
+                    }
+                }
+
+                @Override
+                public void running() {
+                    for (int i = 0; i < distance.length ; i++) {
+                        if (jumpHeight > distance[i]) {
+                            System.out.println(name + " пробежал дистанцию");
+                        } else {
+                            System.out.println(name + " не смог :(");
+                            break;
+                        }
+                    }
+                }
+}
---
+++ src/ru/hw_1/IRunning.java
@@ -0,0 +1,7 @@
+package ru.hw_1;
+
+public interface IRunning {
+
+    void running();
+    int distance[] = new int[]{ 150, 200 , 150 , 150 , 150};
+}
